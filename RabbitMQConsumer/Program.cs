99c0b509e563abc75fd73f80291a0476a4dfff55
Enes Aysan <enesaysan8@gmail.com>
2018-11-24 13:26:46 +0300

    Initial Commit

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RabbitMQConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(1000); // wait one second and get message
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "EnesAys",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    User user = JsonConvert.DeserializeObject<User>(message);
+                    Console.WriteLine($" Adı: {user.Name} Mesaj [{user.Message}]");
+                };
+                channel.BasicConsume(queue: "EnesAys",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Mesaj kuyruktan alındı. Dashboarda bakın.");
+                Console.ReadLine();
+            }
+        }
+    }
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ RabbitMQProducer/Program.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User() { Name = "Enes A.", Message = "Rabbit mq ilk giden mesaj" };
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "EnesAys",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = JsonConvert.SerializeObject(user);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "EnesAys",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine($"Gönderilen kişi: {user.Name}");
+            }
+
+            Console.WriteLine(" Mesaj gönderildi");
+            Console.ReadLine();
+        }
+
+    }
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+    }
+}
